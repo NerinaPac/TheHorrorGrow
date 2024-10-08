9895b6ab58d39967fabf1e89ce2f5b6fe29c7b77
Nerina <paccner@gmail.com>
2024-10-08 17:50:43 -0300

    navegacion fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,19 @@ function NavBar(){
 
     return(
         <header>
-            <nav>
+            
+            <nav>         
+            <Link className="linkNavBar" to={`/`}>
+                    <h1>The Horror Grow</h1>
+            </Link>       
                 <Link className="linkNavBar" to={`/`}>
                     <li>About</li> 
                 </Link>
-                <Link className="linkNavBar" to={`/`}>
-                    <h1>The Horror Grow</h1>
-                </Link>
+                
                 <Link className="linkNavBar" to={`/products`}>
                     <li>Products</li>
                 </Link>
             </nav>
-
-
         </header>
 
     )
