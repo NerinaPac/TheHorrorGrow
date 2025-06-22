e0ac6e6dec25dc8948eb8013f22ceab52085f3c2
Nerina Paccor <paccner@gmail.com>
2025-06-22 18:59:34 -0300

    Fix de Firebase.

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: apiKey,
+  authDomain: "thehorrorgrow-a30cc.firebaseapp.com",
+  projectId: "thehorrorgrow-a30cc",
+  storageBucket: "thehorrorgrow-a30cc.firebasestorage.app",
+  messagingSenderId: "874329819790",
+  appId: "1:874329819790:web:d3201dc12c8289bb7fd7df",
+  measurementId: "G-E1FQ615ZNN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
